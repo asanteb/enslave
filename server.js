4c254376458e2ef3d11a5eb79064d17aafbc6376
asantebuil <asante.buil@tccd.edu>
2016-07-08 16:05:28 -0500

    Signed-off-by: asantebuil <asante.buil@tccd.edu>

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express'),
+    mongoose = require('mongoose'),
+    http = require("http"),
+    app = express(),
+    server = http.createServer(app);
+
+app.get('/', function (req, res) {
+    res.sendFile(_dirname + 'publix/index.html')
+});
+var configDB = require('./config/database.js');
+
+mongoose.connect(configDB.url);
+
+server.listen(3000);
+require('./live-server.js')(server)
